426686fadbbb5cf584f6ee697054928f87b50ca3
Henrik Hagen Rasmussen <111084@gmail.com>
2015-10-03 09:05:38 +0200

    Project filer.

---
+++ awesome_chat/statechart.cpp
@@ -0,0 +1,9 @@
+/*
+ * statechart.cpp
+ *
+ *  Created on: Oct 3, 2015
+ *      Author: stud
+ */
+
+#include "statechart.h"
+
---
+++ awesome_chat/statechart.h
@@ -0,0 +1,16 @@
+/*
+ * statechart.h
+ *
+ *  Created on: Oct 3, 2015
+ *      Author: stud
+ */
+
+#ifndef STATECHART_H_
+#define STATECHART_H_
+
+#include <boost/statechart/state_machine.hpp>
+#include <boost/statechart/simple_state.hpp>
+
+
+
+#endif /* STATECHART_H_ */

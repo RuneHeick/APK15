5588fde4d772d1bf808d0642d4cdf220ccd49c7e
Michael Rahlff <post@m-rahlff.dk>
2015-10-04 10:43:31 +0200

    add and remove client implemented in room.

--- awesome_chat/room.cpp
+++ awesome_chat/room.cpp
@@ -23,7 +23,29 @@ std::string Room::getName()
     return name_;
 }
 
-void Room::broadcastMsg(std::string)
+//void Room::broadcastMsg(e_msg msg)
+//{
+//    for (auto it = clients_.begin(); it != clients_.end(); it++) {
+//        (*it)->sendMsg(msg);
+//    }
+//}
+//
+void Room::addClient(std::shared_ptr<ClientInfo> client)
 {
-    
+    clients_.push_back(client);
+}
+
+void Room::removeClient(std::shared_ptr<ClientInfo> client)
+{
+    for (auto it = clients_.begin(); it != clients_.end(); ) {
+        
+        if ((*it) == client) {
+            clients_.erase(it);
+            return;
+        }else
+        {
+            // We only increase the iterator if we DONT delete anything
+            it++;
+        }
+    }
 }

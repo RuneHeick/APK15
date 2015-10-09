215c3f032e22b045ff21fba300c229ff62b5c355
Henrik Hagen Rasmussen <111084@gmail.com>
2015-10-10 01:33:57 +0200

    Connection failed exception is read and the message printed to the user.

--- awesome_chat/StateMachine/StateClientConnected.h
+++ awesome_chat/StateMachine/StateClientConnected.h
@@ -20,6 +20,7 @@ struct ClientMessageVisitor : boost::static_visitor<void>
 		Cli::writeChatMsg(event.getSender(), event.getMessage());
 	}
 
+	// Do nothing for all other messages
 	template<typename T>
 	void operator()(T& event) const
 	{
@@ -36,6 +37,8 @@ struct StateClientConnected : sc::state<StateClientConnected, StateClient>
 	StateClientConnected(my_context ctx) : my_base( ctx )
 	{
 		Cli::writeDebugMsg("Enter StateClientConnected.");
+
+		bool connectFailed = false;
 		try
 		{
 			std::string& ip = context<StateClient>().ip;
@@ -48,8 +51,22 @@ struct StateClientConnected : sc::state<StateClientConnected, StateClient>
 			disconnect = m_client->disconnected.connect(boost::bind(&StateClientConnected::OnDisconnect, this, _1));
 			MsgRecived = m_client->recivedPacket.connect(boost::bind(&StateClientConnected::OnMsgRecivd, this, _1, _2));
 		}
+		catch(boost::system::error_code& e)
+		{
+			connectFailed = true;
+			Cli::writeLogMsg(Cli::LOGTYPE_ERROR, "Got boost error_code: " + std::to_string(e.value()) + " : " + e.message() );
+		}
+		catch(std::exception& e)
+		{
+			connectFailed = true;
+			Cli::writeLogMsg(Cli::LOGTYPE_ERROR, std::string("Got exception: ") + e.what() );
+		}
 		catch(...)
 		{
+			connectFailed = true;
+		}
+
+		if(connectFailed) {
 			Cli::writeLogMsg(Cli::LOGTYPE_ERROR, "Unable to connect to server (ip: "+context<StateClient>().ip+" port: "+std::to_string(context<StateClient>().port)+" )");
 			post_event(EvClientDisconnect());
 		}

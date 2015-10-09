83d1847d56a54a0af2e65f34165c7c5640d7a9c9
Henrik Hagen Rasmussen <111084@gmail.com>
2015-10-10 01:31:48 +0200

    A comment about a possible change to the handling of the mutex.

--- awesome_chat/StateMachine/ChatStateMachine.cpp
+++ awesome_chat/StateMachine/ChatStateMachine.cpp
@@ -37,10 +37,29 @@ void ChatStateMachine::dequeue(void)
 		  c.wait(lock);
 		}
 		SMEventVariant val = q.front();
+//		lock.unlock(); // Todo Henrik: Hvis nogen kalder postEvent inde fra state-maskinen vil det være nødvendigt at frigive låsen her. Se evt. eksemplet underneden. Kan ikke helt forstå vi ønsker at stå og tage den lås uden noget delay inden næste loop.
 		PostVisitor vs;
 		boost::apply_visitor( boost::bind(vs, _1, this) , val);
+	//	lock.lock();
 		q.pop();
 	}
+
+//	std::unique_lock<std::mutex> lock(m);
+//	while(running)
+//	{
+//		lock.lock();
+//		if(!q.empty())
+//		{
+//			SMEventVariant val = q.front();
+//			lock.unlock();
+//			PostVisitor vs;
+//			boost::apply_visitor( boost::bind(vs, _1, this) , val);
+//			lock.lock();
+//			q.pop();
+//		}
+//		lock.unlock();
+//		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+//	}
 }
 
 

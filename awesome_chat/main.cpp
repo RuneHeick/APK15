b778f6e1bfa019de1a74d040a19c43fc2dd1f39f
Henrik Hagen Rasmussen <111084@gmail.com>
2015-10-19 21:45:36 +0200

    Fjernet exit kommando og fjernet udkommenteret test kode.

--- awesome_chat/main.cpp
+++ awesome_chat/main.cpp
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <signal.h>
 #include <execinfo.h>
-//#include "messageParser.h"
 #include "Network/clientInfo.h"
 #include "StateMachine/StateMachineWrapper.h"
 
@@ -38,48 +37,20 @@ int main(){
 	// Signal handler to catch segmentation faults:
 	signal(SIGSEGV, backTrace_sighandler);
 
-//	EventSerilizer test;
-//	MessageParser mp;
-//
-//    EventVariant var = mp.createEventFromInput("Rune", "/join IHA");
-
-
-//	ClientInfo<IList> ci( c );
-//	std::cout<<"Client Info created"<<std::endl;
-//	EventSerilizer<IList> serilizer;
-//	std::shared_ptr<RawPacket> packet = serilizer.serilize(Join());
-//
-//	Join rune;
-//	ClientInfo<IList>::TVariant var = rune;
-//
-//	std::cout<<"Sending"<<std::endl;
-//	ci.Send(var);
-//
-//
-//	usleep(10000);
-//
-//	running = false;
-//	c.disconnect();
-//	t1.join();
-
 	// Create and initiate the state machine
 	StateMachine::StateMachineWrapper statemachine;
 
 	// Function pointer to handle input
 	std::function<void(const std::string&)> inputHandlerFunc = std::bind(&StateMachine::StateMachineWrapper::HandleUserInput, &statemachine, std::placeholders::_1);
-	bool stopLoop = false;
 
 	// Start reading user intput
-	while(!stopLoop)	{
+	while(true)	{
 		std::string usrInput = Cli::getUserInput();
-		if(usrInput.compare("/exit") == 0) {
-			stopLoop = true;
-		} else {
-			if(inputHandlerFunc)
-				inputHandlerFunc(usrInput);
-		}
+
+		if(inputHandlerFunc)
+			inputHandlerFunc(usrInput);
+
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-	Cli::writeDebugMsg(">> main exit <<");
 }
 
